0bda3370eba0b0e164e5a8600cf188c42331f633
Alanssrv <alanssrv@gmail.com>
2023-06-27 21:52:14 -0300

    atualizando item

--- js/main.js
+++ js/main.js
@@ -2,9 +2,14 @@ const form = document.getElementById('novoItem');
 const lista = document.getElementById('lista');
 const itens = JSON.parse(localStorage.getItem('itens')) || [];
 
+
+
+
 itens.forEach(item => {
     criarElemento(item);
 });
+
+
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -12,23 +17,31 @@ form.addEventListener('submit', (e) => {
     const nome = e.target.elements['nome'];
     const quantidade = e.target.elements['quantidade'];
 
-    const item = {
+    const novoItem = {
         'nome': nome.value,
         'quantidade': quantidade.value
     };
 
-    if (!item.nome || !item.quantidade) {
+    if (!novoItem.nome || !novoItem.quantidade) {
         alert('Insira valores nos campos nome e quantidade');
         return;
     }
 
-    criarElemento(item);
-    itens.push(item);
+    const itemExistente = itens.find(it => it.nome === novoItem.nome);
+
+    if (itemExistente) {
+        novoItem.id = itemExistente.id;
+        atualizarElemento(novoItem);
+        itens[novoItem.id] = novoItem;
+    } else {
+        novoItem.id = itens.length;
+        criarElemento(novoItem);
+        itens.push(novoItem);
+    }
     localStorage.setItem('itens', JSON.stringify(itens));
     
     nome.value = '';
     quantidade.value = '';
-
 });
 
 function criarElemento(item) {
@@ -36,10 +49,16 @@ function criarElemento(item) {
     novoItem.classList.add('item');
 
     const quantidadeItem = document.createElement('strong');
+    quantidadeItem.dataset.id = item.id;
     quantidadeItem.textContent = item.quantidade;
     
     novoItem.appendChild(quantidadeItem);
     novoItem.innerHTML += item.nome;
     
     lista.appendChild(novoItem);
+}
+
+function atualizarElemento(item) {
+    const itemLista = document.querySelector(`[data-id="${item.id}"]`);
+    itemLista.innerHTML = item.quantidade;
 }
